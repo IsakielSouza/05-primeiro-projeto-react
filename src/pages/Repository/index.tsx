3f3c84d79d7a8b6cf01a05fc01e367096ebfa5f5
Isakiel Souza <isakielsouza@outlook.com.br>
2020-05-17 05:33:28 -0300

    Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/46832485?s=460&u=cec3aa86925eac02af1870039e078df9671f22a8&v=4"
+            alt="Isakiel Souza"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Descrição do repository</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="adsfasdf">
+          <div>
+            <strong>adfadsf</strong>
+            <p>adfadfasdf</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
